d6e7cb5736e9b27e489df025a18ed46745877dfe
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-23 04:19:52 +0900

    internal/cmd/mmalgen: support generate function call
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- internal/cmd/mmalgen/main.go
+++ internal/cmd/mmalgen/main.go
@@ -30,11 +30,25 @@ var builtinTypes = map[string]string{
 	"unsigned int": "uint",
 }
 
-var templ = `package ` + strings.ToLower(prefix)
+var header = os.Args[1]
+var templ = `package ` + strings.ToLower(prefix) + `
+
+/*
+#include <` + trimIncludeDir(header) + `>
+*/
+import "C"`
+
+func trimIncludeDir(d string) string {
+	var dd []string
+	for _, sep := range []string{"userland/", "include/"} {
+		if strings.Contains(d, sep) {
+			dd = strings.Split(d, sep)
+		}
+	}
+	return dd[1]
+}
 
 func main() {
-	header := os.Args[1]
-
 	idx := clang.NewIndex(0, 0)
 	defer idx.Dispose()
 
@@ -48,7 +62,6 @@ func main() {
 	firstEnum := false
 	visitor := func(cursor, parent clang.Cursor) clang.ChildVisitResult {
 		spelling := cursor.Spelling()
-
 		if cursor.IsNull() {
 			return clang.ChildVisit_Continue
 		}
@@ -57,13 +70,37 @@ func main() {
 		}
 
 		switch kind := cursor.Kind(); kind {
+		case clang.Cursor_FunctionDecl:
+			buf.WriteString(fmt.Sprintf("func %s(", UpperCase(spelling)))
+			numArgs := cursor.NumArguments()
+			for i := uint32(0); i < uint32(numArgs); i++ {
+				buf.WriteString(fmt.Sprintf("%s %s", cursor.Argument(i).Spelling(), formatType(cursor.Argument(i).Type().Spelling())))
+				if i+1 < uint32(numArgs) {
+					buf.WriteString(", ")
+				}
+			}
+			resType := UpperCase(cursor.ResultType().Spelling())
+			buf.WriteString(fmt.Sprintf(") %s {\n", resType))
+			buf.WriteString(fmt.Sprintf("\treturn "+resType+"(C."+cursor.Spelling()) + "(")
+			for i := uint32(0); i < uint32(numArgs); i++ {
+				arg := cursor.Argument(i).Spelling()
+				switch arg {
+				case "port":
+					arg = "port.c"
+				}
+				buf.WriteString(arg)
+				if i+1 < uint32(numArgs) {
+					buf.WriteString(", ")
+				}
+			}
+			buf.WriteString("))\n")
+			buf.WriteString("}\n\n")
 		case clang.Cursor_StructDecl:
 			if seen[cursor.USR()] {
 				return clang.ChildVisit_Continue
 			}
 			buf.WriteString("type " + UpperCase(spelling) + " struct {\n\tc C." + spelling + "\n}\n\n")
 			seen[cursor.USR()] = true
-		case clang.Cursor_TypedefDecl:
 		case clang.Cursor_FieldDecl:
 			if ptype := parent.Spelling(); ptype != "" {
 				var builtin bool
@@ -107,7 +144,7 @@ func main() {
 				fmt.Println(spelling)
 			}
 		case clang.Cursor_MacroDefinition:
-			fmt.Printf("kind: %s, spelling: %s\n", cursor.Kind(), spelling)
+			// fmt.Printf("kind: %s, spelling: %s\n", cursor.Kind(), spelling)
 			// WriteConst(&buf, cursor)
 		default:
 			// fmt.Printf("kind: %s, spelling: %s\n", cursor.Kind(), spelling)
@@ -130,6 +167,17 @@ func ReceiverName(s string) string {
 
 func TrimLibPrefix(s string) string {
 	return strings.TrimPrefix(s, prefix+"_")
+}
+
+func formatType(s string) string {
+	s = strings.TrimPrefix(s, prefix+"_")
+	if v, ok := builtinTypes[s]; ok {
+		return v
+	}
+	if strings.HasSuffix(s, " *") {
+		s = strings.Replace(s, " *", "", 1)
+	}
+	return UpperCase(s)
 }
 
 // LowerCase returns s with the first character lower-cased. LowerCase
@@ -163,10 +211,13 @@ func UpperCase(s string) string {
 	if len(s) == 0 {
 		return s
 	}
+	if v, ok := builtinTypes[s]; ok {
+		return v
+	}
 	var buf bytes.Buffer
 	ss := strings.Split(s, "_")
 	for _, n := range ss {
-		if n == prefix {
+		if n == prefix || n == strings.ToLower(prefix) {
 			continue
 		}
 		if len(n) == 1 {
