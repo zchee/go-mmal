4491c78ecdb26508e22f358f4bbf92194e96b751
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-22 09:45:46 +0900

    cgoflags: enable raspberry pi native pkg-config #cgo pragma
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cgoflags.go
+++ cgoflags.go
@@ -6,9 +6,9 @@ package mmal
 
 /*
 // for local environment
-#cgo CFLAGS: -I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads
-#cgo LDFLAGS: -L/opt/vc/lib -lmmal -lmmal_core -lmmal_util -lmmal_vc_client -lbcm_host -lvcsm
+// #cgo CFLAGS: -I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads
+// #cgo LDFLAGS: -L/opt/vc/lib -lmmal -lmmal_core -lmmal_util -lmmal_vc_client -lbcm_host -lvcsm
 // for raspberry pi native
-// #cgo pkg-config: mmal
+#cgo pkg-config: mmal
 */
 import "C"
