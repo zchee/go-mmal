0b218a20650f814e04d1872b549bfc51ecd8a8c5
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-23 16:01:53 +0900

    doc: add package description
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 The go-mmal Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package mmal implements Raspberry Pi's libmmal bindings for Go.
+package mmal
