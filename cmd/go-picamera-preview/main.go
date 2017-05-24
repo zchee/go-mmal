bab14b1a99caaeb5e03ef41b63fdb05c412793d3
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-24 18:55:00 +0900

    cmd/go-picamera-preview: add go-picamera-preview command
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ cmd/go-picamera-preview/main.go
@@ -0,0 +1,112 @@
+// Copyright 2017 Koichi Shiraishi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// Copyright (c) 2017 Sugizaki Yukimasa (ysugi@idein.jp)
+// All rights reserved.
+// 
+// This software is licensed under a Modified (3-Clause) BSD License.
+// You should have received a copy of this license along with this
+// software. If not, contact the copyright holder above.
+
+// This code and comment are based by C source from:
+//   https://github.com/Terminus-IMRC/rpi_cam_previewer/blob/f09489f164de3ece95f5e6c222995798d8bc379a/rpi_cam_previewer.c
+// Thanks @Terminus-IMRC for publish simple camera preview source.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"unsafe"
+
+	mmal "github.com/zchee/go-mmal"
+)
+
+const CameraNum = 0
+
+var (
+	previewDestRect = mmal.NewRect(1024/4, 768/4, 1024/2, 768/4)
+	// TODO(zchee): implements NewParameterFPSRange()
+	// previewFPSRange = mmal.ParameterFPSRange{}
+)
+
+const (
+	previewWidth  = 1024
+	previewHeight = 768
+)
+
+var (
+	wait = flag.Int("time", 5000, "time for preview")
+)
+
+func main() {
+	flag.Parse()
+
+	mmal.BCMHostInit()
+
+	// create camera and video_render component
+	var cpCamera mmal.WrapperType
+	if err := mmal.WrapperCreate(&cpCamera, mmal.ComponentDefaultCamera); err != mmal.Success {
+		log.Fatalf("go-picamera: %v", err)
+	}
+	var cpVideoRenderor mmal.WrapperType
+	if err := mmal.WrapperCreate(&cpVideoRenderor, mmal.ComponentDefaultVideoRendereu); err != mmal.Success {
+		log.Fatalf("go-picamera: %v", err)
+	}
+
+	// control port of camera
+	control := cpCamera.Control()
+	prmhdr := mmal.NewParameterHeader(uint32(mmal.ParameterCameraNum), uint32(unsafe.Sizeof(mmal.ParameterInt32Type{})))
+	cameraNum := mmal.NewParameterInt32Type(prmhdr, CameraNum)
+	mmal.PortParameterSet(&control, cameraNum.Hdr())
+
+	// preview port of camera
+	output := cpCamera.Output()
+	mmal.SetPortFormat(&output)
+	
+	// TODO(zchee): omit set port parameter for now
+	// set fps
+	// mmal_port_parameter_set(output, &preview_fps_range.hdr)
+
+	// input port of video_renderer
+	input := cpVideoRenderor.Input()
+	drhdr := mmal.NewParameterHeader(uint32(mmal.ParameterDisplayregion), uint32(unsafe.Sizeof(mmal.Displayregion{})))
+	dr := mmal.NewDisplayregion(&drhdr, uint32(mmal.DisplaySetDestRect|mmal.DisplaySetFullscreen), false, previewDestRect)
+	if err := mmal.PortParameterSet(&input, dr.Hdr()); err != mmal.Success {
+		log.Fatalf("go-picamera: %v", err)
+	}
+
+	// create connection between camera's preview and video_renderer
+	var connection mmal.ConnectionType
+	if err := mmal.ConnectionCreate(&connection, cpCamera.Output(), cpVideoRenderor.Input(), mmal.ConnectionFlagTunnelling|mmal.ConnectionFlagAllocationOnInput); err != mmal.Success {
+		log.Fatalf("go-picamera: %v", err)
+	}
+	// and enable it
+	if err := mmal.ConnectionEnable(&connection); err != mmal.Success {
+		log.Fatalf("go-picamera: %v", err)
+	}
+
+	destroy := func() {
+		mmal.ConnectionDestroy(&connection)
+		mmal.WrapperDestroy(&cpVideoRenderor)
+		// TODO(zchee): occur "double free or corruption" error if destroy. Because already cleanuped from Go garbage collection?
+		// mmal.WrapperDestroy(&cpCamera)
+		mmal.BCMHostDeinit()
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sig
+		destroy()
+		os.Exit(1)
+	}()
+
+	// preview for "wait" seconds
+	mmal.VcosSleep(uint32(*wait))
+	destroy()
+}
