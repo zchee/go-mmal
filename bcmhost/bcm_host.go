bd0d937ca1119fb97566cb9f9657adc02e76d372
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-24 20:08:54 +0900

    bcmhost: move bcm_host to bcmhost package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- bcmhost/bcm_host.go
+++ bcmhost/bcm_host.go
@@ -2,18 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package mmal
+package bcmhost
 
 /*
+#cgo pkg-config: bcm_host
 #include <bcm_host.h>
 */
 import "C"
 
-func BCMHostInit() {
+func Init() {
 	C.bcm_host_init()
 }
 
-func BCMHostDeinit() {
+func Deinit() {
 	C.bcm_host_deinit()
 }
 
@@ -23,14 +24,14 @@ func GraphicsGetDisplaySize(displayNumber uint16, width uint32, height uint32) i
 	return int32(C.graphics_get_display_size(C.uint16_t(displayNumber), &w, &h))
 }
 
-func BCMHostGetPeripheralAddress() uint {
+func GetPeripheralAddress() uint {
 	return uint(C.bcm_host_get_peripheral_address()) 
 }
 
-func BCMHostGetPeripheralSize() uint {
+func GetPeripheralSize() uint {
 	return uint(C.bcm_host_get_peripheral_size()) 
 }
 
-func BCMHostGetSdramAddress() uint {
+func GetSdramAddress() uint {
 	return uint(C.bcm_host_get_sdram_address()) 
 }
