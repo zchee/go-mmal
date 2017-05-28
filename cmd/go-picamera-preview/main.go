96cc3723e6964bc330a3a2cb48ec163752f8c15b
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-28 22:39:42 +0900

    cmd/go-picamera-preview: fix error handling to nil
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/go-picamera-preview/main.go
+++ cmd/go-picamera-preview/main.go
@@ -42,11 +42,11 @@ func main() {
 
 	// create camera and video_render component
 	var cpCamera mmal.WrapperType
-	if err := mmal.WrapperCreate(&cpCamera, mmal.ComponentDefaultCamera); err != mmal.Success {
+	if err := mmal.WrapperCreate(&cpCamera, mmal.ComponentDefaultCamera); err != nil {
 		log.Fatalf("go-picamera: %v", err)
 	}
 	var cpVideoRenderor mmal.WrapperType
-	if err := mmal.WrapperCreate(&cpVideoRenderor, mmal.ComponentDefaultVideoRendereu); err != mmal.Success {
+	if err := mmal.WrapperCreate(&cpVideoRenderor, mmal.ComponentDefaultVideoRendereu); err != nil {
 		log.Fatalf("go-picamera: %v", err)
 	}
 
@@ -58,7 +58,7 @@ func main() {
 	input := cpVideoRenderor.Input()
 	drhdr := mmal.NewParameterHeader(uint32(mmal.ParameterDisplayregion), uint32(unsafe.Sizeof(mmal.Displayregion{})))
 	dr := mmal.NewDisplayregion(&drhdr, uint32(mmal.DisplaySetDestRect|mmal.DisplaySetFullscreen), false, previewDestRect)
-	if err := mmal.PortParameterSet(&input, dr.Hdr()); err != mmal.Success {
+	if err := mmal.PortParameterSet(&input, dr.Hdr()); err != nil {
 		log.Fatalf("go-picamera: %v", err)
 	}
 
@@ -66,11 +66,11 @@ func main() {
 	var connection mmal.ConnectionType
 	cameraOutput := cpCamera.Output()
 	videoInput := cpVideoRenderor.Input()
-	if err := mmal.ConnectionCreate(&connection, &cameraOutput, &videoInput, mmal.ConnectionFlagTunnelling|mmal.ConnectionFlagAllocationOnInput); err != mmal.Success {
+	if err := mmal.ConnectionCreate(&connection, &cameraOutput, &videoInput, mmal.ConnectionFlagTunnelling|mmal.ConnectionFlagAllocationOnInput); err != nil {
 		log.Fatalf("go-picamera: %v", err)
 	}
 	// and enable it
-	if err := mmal.ConnectionEnable(&connection); err != mmal.Success {
+	if err := mmal.ConnectionEnable(&connection); err != nil {
 		log.Fatalf("go-picamera: %v", err)
 	}
 
