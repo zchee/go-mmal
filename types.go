8f845a8f445250c18ee22cb744a4b154d3a49694
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-28 21:34:12 +0900

    types: implements String and Error interface for Status
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- types.go
+++ types.go
@@ -8,6 +8,9 @@ package mmal
 #include <interface/mmal/mmal_types.h>
 */
 import "C"
+import (
+	"fmt"
+)
 
 type Status C.MMAL_STATUS_T
 
@@ -30,6 +33,86 @@ const (
 	ErrFault
 	ErrMax = 0x7FFFFFFF
 )
+
+func (s Status) String() string {
+	switch s {
+	case Success:
+		return "Success"
+	case ErrNoMem:
+		return "ErrNoMem"
+	case ErrNoSpc:
+		return "ErrNoSpc"
+	case ErrInVal:
+		return "ErrInVal"
+	case ErrNoSys:
+		return "ErrNoSys"
+	case ErrNoEnt:
+		return "ErrNoEnt"
+	case ErrNXIO:
+		return "ErrNXIO"
+	case ErrIO:
+		return "ErrIO"
+	case ErrSPipe:
+		return "ErrSPipe"
+	case ErrCorrupt:
+		return "ErrCorrupt"
+	case ErrNotReady:
+		return "ErrNotReady"
+	case ErrConfig:
+		return "ErrConfig"
+	case ErrIsConn:
+		return "ErrIsConn"
+	case ErrNotConn:
+		return "ErrNotConn"
+	case ErrAgain:
+		return "ErrAgain"
+	case ErrFault:
+		return "ErrFault"
+	case ErrMax:
+		return "ErrMax"
+	}
+	return fmt.Sprintf("Status(%d)", s)
+}
+
+func (s Status) Error() string {
+	switch s {
+	case Success:
+		return "Success"
+	case ErrNoMem:
+		return "Out of memory"
+	case ErrNoSpc:
+		return "Out of resources (other than memory)"
+	case ErrInVal:
+		return "Argument is invalid"
+	case ErrNoSys:
+		return "Function not implemented"
+	case ErrNoEnt:
+		return "No such file or directory"
+	case ErrNXIO:
+		return "No such device or address"
+	case ErrIO:
+		return "I/O error"
+	case ErrSPipe:
+		return "Illegal seek"
+	case ErrCorrupt:
+		return "Data is corrupt"
+	case ErrNotReady:
+		return "Component is not ready"
+	case ErrConfig:
+		return "Component is not configured"
+	case ErrIsConn:
+		return "Port is already connected"
+	case ErrNotConn:
+		return "Port is disconnected"
+	case ErrAgain:
+		return "Resource temporarily unavailable"
+	case ErrFault:
+		return "Bad address"
+	case ErrMax:
+		return "Force to 32 bit"
+	}
+	return "unknown error"
+}
 
 type Rect struct {
 	c C.MMAL_RECT_T
